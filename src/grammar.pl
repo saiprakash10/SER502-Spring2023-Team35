787cfe8c03fe256019c9c57e44da6f1276e14680
Ryan Collins <ryan@Ryans-MacBook-Pro.local>
2023-04-23 23:51:11 -0700

    t push
    z
    
    x
    
    `
    ~
    a
    -a
    
    :wq

--- src/grammar.pl
+++ src/grammar.pl
@@ -49,3 +49,40 @@ If_then_else::=  if B { C }
                 | if B { C } elif { C } Elif else { C } 
  
 Elif ::= elif { C } Elif | ε 
+
+while_loop::=  while B { C }
+ 
+for_loop::=  for I in range (N,N){ C }
+            | for(I = N; I S N; ID){ C }
+ 
+ternary_expression::= ( C ) ? E : E
+
+print_statement::= print(E)
+                  |print(I)
+                  |print(ST)
+                  |print(B)
+ 
+B ::=     BI | E S E | not B | B and B| B or B
+             
+E ::=     T + E | T - E | T
+T ::=     ( E ) T
+T ::=     F * T | F / T | F
+F ::=     I | N
+Bl ::=    true | false
+N ::=    DG, N | ε
+ST ::=  CH, ST | ε
+DG ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+CH ::=  Upper_case
+        | Lower_case
+        | special_char
+
+
+Upper_case ::= A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
+
+Lower_case ::= a | b | c | d | e | f | g | h | i | j | k | l | m | n | o | p | q | r | s | t | u | v | w | x | y | z
+
+special_char ::= ! | " | # | $ | % | & | ' | ( | ) | * | + | , | - | . | / | : | ; | < | = | > | ? | @ | [ | \ | ] | ^ | _ | ` | { | | | } | ~
+
+
+S ::=     < | > | >= | <= | == | !=
+ID ::=   I++ | I--
