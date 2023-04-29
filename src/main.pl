e500a92bf4bc47eb18b6202fba79a475b23506a6
saiprakashasu <sravic24@asu.edu>
2023-04-28 19:31:57 -0700

    Create main.pl

---
+++ src/main.pl
@@ -0,0 +1,17 @@
+:- use_module(token_reader).
+:- use_module(parser).
+:- use_module(evaluator).
+
+main(Filename) :- nl,
+    ansi_format([bold,fg(yellow)], 'Parser in progress', []), nl,
+    read_file(Filename, FileData),
+    program(ParseTree, FileData, []),
+    write("Parse Tree generation in progress: "), nl,
+    write(ParseTree), nl,
+    ansi_format([bold,fg(yellow)], 'Evaluation in progress', []), nl,
+    eval_program(ParseTree, NewEnv), nl,
+    ansi_format([bold,fg(yellow)], 'Environment after evaluation', []), nl,
+    write(NewEnv), nl,
+    halt.
+
+
